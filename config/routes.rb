b42d1ef9576d6ff2ef337d1143f831ea2ddbd223
vagrant <vagrant@localhost.localdomain>
2020-06-02 05:53:47 +0000

    [Update route and controller]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root 'home#top'
   get 'home/about' => 'home#about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resource :favorites, only: [:create, :destroy]
-  resources :books
+  resources :books do
+  	resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+  end
 end
